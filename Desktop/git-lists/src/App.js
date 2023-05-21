0dcd8192c389d8fb1f4175451631f02a6dd0fd35
Dharmendra Singh <dharsin2@cisco.com>
2023-05-21 05:57:47 -0400

    adding possible routes and some code changes to test initial pages

--- Desktop/git-lists/src/App.js
+++ Desktop/git-lists/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { Search } from './components/Search';
+import { Profile } from './components/Profile';
+import React from 'react';
+import {BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import {useState, createContext } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+  const [userInput,setUserInput] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Git Lists" subtitle="" />
+      {/* useContext hook is used here to pass the userInput and setUserInput through the pages. */}
+       <AppContext.Provider value={{userInput,setUserInput}}>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Search />} />
+                <Route path="/:id" element={<Search />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/profile/:id" element={<Profile />} />
+            </Routes>
+        </Router>
+      </AppContext.Provider>
+      <Footer note="Simple App to list user repos by Dharm" />
     </div>
   );
 }
