b56272f82ca38b2fcaab71c370d997ef50539bb9
Dharmendra Singh <dharsin2@cisco.com>
2023-05-22 03:12:53 -0400

    changing footer note

--- Desktop/git-lists/src/utility/constanst.js
+++ Desktop/git-lists/src/utility/constanst.js
@@ -8,7 +8,7 @@ export const LIST_CONSTANTS = {
     MSG_NOTFOUND: 'Not Found',
     MSG_TOOLTIP_URL: 'Enter GIT UserID at end of URL like : http://localhost:3000/pdf or https://calldharm.github.io/git-lists/pdf ',
     MSG_VIEWREPOOF: 'Viewing repositories of',
-    MSG_FOOTER: 'Simple App to list user repos by Dharm',
+    MSG_FOOTER: 'Simple App to list user git repos (developer: dharm, 2023)',
     MSG_HEADER_TITLE: 'Git Lists',
     MSG_HEADER_SUBTITLE: ''
 };
